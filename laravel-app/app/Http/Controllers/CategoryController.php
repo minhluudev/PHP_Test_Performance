805469a3ac0719ddd769adefb52234cd8465698a
Minh Luu <minhluuth@gmail.com>
2025-09-19 21:10:56 +0700

    update api

---
+++ laravel-app/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use App\Models\Post;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Post::with('category')
+                        ->where('title', 'like','% %')
+                        ->where('description', 'like','% %')
+                        ->where('content', 'like','% %')
+                        ->orderBy('title', 'asc')
+                        ->orderBy('description', 'asc')
+                        ->orderBy('content', 'desc')
+                        ->paginate(200);
+
+        return response()->json($products);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('categories', CategoryController::class);
